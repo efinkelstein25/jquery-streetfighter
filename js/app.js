8279964794c484addefb4837af4f5cec55985a24
efinkelstein25 <estherfinkelstein@Esthers-MacBook-Air.local>
2014-06-23 21:59:45 -0400

    Add animation/sound

--- js/app.js
+++ js/app.js
@@ -11,19 +11,30 @@ $(document).ready(function(){
   })
 
   .mousedown(function(){
-  		//play sounds
+  		playHadouken();
   		$('.ryu-ready').hide();
   		$('.ryu-throwing').show();
-  		$('.hadouken').show();
-  		//show hadouken and animate it to the right of screen
+  		$('.hadouken').finish().show()
+  		.animate(
+  			{'left': '300px'},
+  			500,
+  			function(){
+  				$(this).hide();
+  				$(this).css('left', '-212px');
 
+  			}
+
+  		);
   })
   .mouseup(function(){
   		$('.ryu-throwing').hide();
   		$('.ryu-ready').show();
   });
-
-
-
 });
+
+function playHadouken(){
+	$('#hadouken-sound')[0].volume = .05;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
 
