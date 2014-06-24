2e67f14936a81809061dbd595b7198536cd11f8f
efinkelstein25 <estherfinkelstein@Esthers-MacBook-Air.local>
2014-06-23 21:43:46 -0400

    Adding movement to Ryu

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+	$('.ryu').mouseenter(function(){
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+
+	})
+	.mouseleave(function() {
+    	$('.ryu-ready').hide();
+		$('.ryu-still').show();
+  })
+
+  .mousedown(function(){
+  		//play sounds
+  		$('.ryu-ready').hide();
+  		$('.ryu-throwing').show();
+  		$('.hadouken').show();
+  		//show hadouken and animate it to the right of screen
+
+  })
+  .mouseup(function(){
+  		$('.ryu-throwing').hide();
+  		$('.ryu-ready').show();
+  });
+
+
+
+});
+
